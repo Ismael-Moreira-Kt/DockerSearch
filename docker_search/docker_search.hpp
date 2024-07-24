008ab71d07adfdcd1c0545ad040d77e292e5f77b
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-07-24 09:34:38 +0100

    feat: I've added the library's main header.
    I have added the definition of the DockerSearch class together with two private members.

---
+++ docker_search/docker_search.hpp
@@ -0,0 +1,19 @@
+#ifndef DOCKER_SEARCH_HPP
+#define DOCKER_SEARCH_HPP
+
+
+
+#include "_include.hpp"
+
+
+
+class DockerSearch {
+    private:
+        std::string path;
+        std::ofstream fileStream;
+    public:
+};
+
+
+
+#endif
