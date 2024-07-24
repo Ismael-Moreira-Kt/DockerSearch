fe71ec29981dbe49146ce32129c10b1d90cc5563
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-07-24 15:44:41 +0100

    feat: Added the header that lists all the tests.

---
+++ _tests/tests.hpp
@@ -0,0 +1,14 @@
+#ifndef TESTS_HPP
+#define TESTS_HPP
+
+
+
+void TestConvertToLowerPositive();
+void TestConvertToLowerAlreadyLowercase();
+void TestConvertToLowerEmpty();
+void TestConvertToLowerMixedCase();
+void TestConvertToLowerNonAlphabetic();
+
+
+
+#endif
