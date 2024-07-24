43bea22b1420581bb5b582fa3e4b98f540c372d1
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-07-24 09:52:59 +0100

    feat: Addition of the header and implementation of the start_search function.
    The function is responsible for managing all searches for docker images.

--- docker_search/docker_search.cpp
+++ docker_search/docker_search.cpp
@@ -5,3 +5,21 @@
 void DockerSearch::define_path(std::string& path) {
     this -> path = path;
 }
+
+
+
+void DockerSearch::start_search(std::string& name) {
+    if (this -> path.empty()) {
+        this -> path = ".";
+    }
+
+    if (name.empty()) {
+        throw std::runtime_error("Name cannot be empty.");
+    }
+
+    try {
+
+    } catch(const std::runtime_error& error) {
+        std::cerr << "Search process stopped: " << error.what() << std::endl;
+    }
+}
